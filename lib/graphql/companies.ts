8161f21d5cb7afb5787e7104dd49a75afd06558f
0stoya <chris@ostoya.co.uk>
2026-09-05 22:37:06 +0100

    Remove obsolete company availability probe

--- lib/graphql/companies.ts
+++ lib/graphql/companies.ts
@@ -1,4 +1,4 @@
-import { graphqlPartialRequest, graphqlRequest } from "@/lib/graphql/client";
+import { graphqlRequest } from "@/lib/graphql/client";
 
 export type CompanySummary = {
   company_id: number;
@@ -19,16 +19,6 @@ export type CompanyList = {
 
 type CompanyListData = { css_admin_companies: CompanyList };
 type CompanyDetailData = { css_admin_company: CompanySummary };
-type Typename = { __typename: string };
-type ManagementAccess = { company_id: number };
-type ManagementProbeData = {
-  management: ManagementAccess | null;
-  catalog: Typename | null;
-  purchase_controls: Typename | null;
-  commercial: Typename | null;
-  credit: Typename | null;
-  credit_orders: Typename | null;
-};
 
 const COMPANY_LIST_QUERY = /* GraphQL */ `
   query AdminCompanyList($currentPage: Int!, $pageSize: Int!) {
@@ -60,17 +50,6 @@ const COMPANY_DETAIL_QUERY = /* GraphQL */ `
   }
 `;
 
-const MANAGEMENT_PROBE_QUERY = /* GraphQL */ `
-  query AdminCompanyManagementAvailability($companyId: Int!) {
-    management: css_admin_company_management(company_id: $companyId) { company_id }
-    catalog: css_admin_company_catalog_policy(company_id: $companyId) { __typename }
-    purchase_controls: css_admin_purchase_controls(company_id: $companyId) { __typename }
-    commercial: css_admin_company_payment_configuration(company_id: $companyId) { __typename }
-    credit: css_admin_company_credit(company_id: $companyId) { __typename }
-    credit_orders: css_admin_credit_orders(company_id: $companyId, currentPage: 1, pageSize: 1) { __typename }
-  }
-`;
-
 export async function getCompanies(currentPage = 1, pageSize = 100) {
   const data = await graphqlRequest<CompanyListData, { currentPage: number; pageSize: number }>(
     COMPANY_LIST_QUERY,
@@ -85,21 +64,4 @@ export async function getCompany(companyId: number) {
     { companyId },
   );
   return data.css_admin_company;
-}
-
-export async function getCompanyManagementAvailability(companyId: number) {
-  const response = await graphqlPartialRequest<ManagementProbeData, { companyId: number }>(
-    MANAGEMENT_PROBE_QUERY,
-    { companyId },
-  );
-
-  const data = response.data;
-  return {
-    management: Boolean(data?.management),
-    catalog: Boolean(data?.catalog),
-    purchase_controls: Boolean(data?.purchase_controls),
-    commercial: Boolean(data?.commercial),
-    credit: Boolean(data?.credit),
-    credit_orders: Boolean(data?.credit_orders),
-  };
 }
